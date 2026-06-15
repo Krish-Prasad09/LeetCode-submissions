c7536e18236430622a89d90deb8e01600c03d178
Krish Prasad <prasadkrish9306@gmail.com>
2026-06-15 13:25:23 +0530

    Time: 2450 ms (5.02%), Space: 55.8 MB (76.52%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -3,7 +3,7 @@ class Twitter {
     int curr=0;
     // priority_queue<vector<int>> timeline; //{time, tweetId, userId}
     unordered_map<int,unordered_set<int>> friends; //Ids are unique
-    unordered_map<int, vector<pair<int,int>>> posts;
+    vector<pair<int,int>> posts;
 public:
     Twitter() {
         // friends.clear();
@@ -13,10 +13,11 @@ public:
     }
     
     void postTweet(int userId, int tweetId) {
-        posts[userId].push_back({curr++, tweetId});
+        posts.push_back({userId,tweetId});
     }
     
     vector<int> getNewsFeed(int userId) {
+        vector<int> ans;
         // priority_queue<vector<int>> userTimeline=timeline;
         int n=0;
         // while(!userTimeline.empty() && n<10){
@@ -28,82 +29,20 @@ public:
         //     }
         // }
 
-        // for(int i=posts.size()-1; i>=0 && n<10; i--){
-        //     if(posts[i].first==userId || friends[userId].count(posts[i].first)){
-        //         ans.push_back(posts[i].second);
-        //         n++;
-        //     }
-        // }
-        // return ans;
-
-        priority_queue<vector<int>> pq;
-        vector<int> ans;
-
-        // user's own latest tweet
-        if (!posts[userId].empty()) {
-            int idx = posts[userId].size() - 1;
-
-            pq.push({
-                posts[userId][idx].first,   // timestamp
-                posts[userId][idx].second,  // tweetId
-                userId,
-                idx
-            });
-        }
-
-        // latest tweet of each followee
-        for (auto followee : friends[userId]) {
-
-            if (!posts[followee].empty()) {
-
-                int idx = posts[followee].size() - 1;
-
-                pq.push({
-                    posts[followee][idx].first,
-                    posts[followee][idx].second,
-                    followee,
-                    idx
-                });
+        for(int i=posts.size()-1; i>=0 && n<10; i--){
+            if(posts[i].first==userId || friends[userId].count(posts[i].first)){
+                ans.push_back(posts[i].second);
+                n++;
             }
         }
-
-        while (!pq.empty() && ans.size() < 10) {
-
-            auto top = pq.top();
-            pq.pop();
-
-            int tweetId = top[1];
-            int uid = top[2];
-            int idx = top[3];
-
-            ans.push_back(tweetId);
-
-            // push previous tweet of same user
-            if (idx > 0) {
-
-                pq.push({
-                    posts[uid][idx - 1].first,
-                    posts[uid][idx - 1].second,
-                    uid,
-                    idx - 1
-                });
-            }
-        }
-
         return ans;
     }
     
     void follow(int followerId, int followeeId) {
-        if (followerId == followeeId)
-            return;
-
         friends[followerId].insert(followeeId);
     }
     
     void unfollow(int followerId, int followeeId) {
-        if (followerId == followeeId)
-            return;
-
         friends[followerId].erase(followeeId);
     }
 };
@@ -116,3 +55,4 @@ public:
  * obj->follow(followerId,followeeId);
  * obj->unfollow(followerId,followeeId);
  */
+
