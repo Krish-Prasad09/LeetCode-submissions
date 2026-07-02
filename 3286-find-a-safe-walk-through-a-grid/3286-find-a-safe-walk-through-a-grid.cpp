057c19ae5376b5750d9e465efe37ddf423ec4078
Krish Prasad <prasadkrish9306@gmail.com>
2026-07-02 13:24:09 +0530

    Time: 14 ms (74.11%), Space: 30.3 MB (94.3%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -2,33 +2,28 @@ class Solution {
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int row = grid.size(), col = grid[0].size();
-        queue<pair<int, pair<int, int>>> q;
-        if (grid[0][0] == 1)
-            health--;
-        if (health <= 0)
-            return false; // 1. MANDATORY: Track max health per cell to stop the
-                          // infinite loops causing your MLE
-        vector<vector<int>> maxh(row, vector<int>(col, -1));
-        q.push({health, {0, 0}});
-        maxh[0][0] = health;
-        while (!q.empty()) {
-            int h = q.front().first;
-            int ri = q.front().second.first;
-            int ci = q.front().second.second;
-            int delr[] = {-1, 0, 1, 0};
-            int delc[] = {0, 1, 0, -1};
-            q.pop();
-            if (ri == row - 1 && ci == col - 1 && h >= 1)
-                return true;
-            for (int i = 0; i < 4; i++) {
-                int newr = delr[i] + ri;
-                int newc = delc[i] + ci;
-                if (newr < row && newr >= 0 && newc < col && newc >= 0) {
-                    int next_h = h - grid[newr][newc];
-
-                    if (next_h > maxh[newr][newc] && next_h > 0) {
-                        maxh[newr][newc] = next_h;
-                        q.push({next_h, {newr, newc}});
+        int delr[] = {-1, 0, 1, 0};
+        int delc[] = {0, 1, 0, -1};
+        vector<vector<int>>cost(row,vector<int>(col,INT_MIN));
+        cost[0][0]=health-grid[0][0];
+        priority_queue<pair<int,pair<int,int>>> pq;
+        pq.push({cost[0][0],{0,0}});
+        while(!pq.empty()){
+            auto it=pq.top();
+            pq.pop();
+            int h=it.first;
+            int ri=it.second.first;
+            int ci=it.second.second;
+            if(h<1) return false;
+            if(ri==row-1 && ci==col-1) return true;
+            for(int i=0; i<4; i++){
+                int newi=delr[i]+ri;
+                int newc=delc[i]+ci;
+                if(newi>=0 && newc>=0 && newi<row && newc<col){
+                    int val=h-grid[newi][newc];
+                    if(val>cost[newi][newc]){
+                        cost[newi][newc]=val;
+                        pq.push({val,{newi,newc}});
                     }
                 }
             }
