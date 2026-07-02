896662b72481b37c1f19ad0a3b3e27ea36674c40
Krish Prasad <prasadkrish9306@gmail.com>
2026-07-02 12:48:16 +0530

    Time: 26 ms (44.38%), Space: 36.9 MB (36.52%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
-bool f(int ri, int ci, int health, vector<vector<int>> &grid, int row, int col,vector<vector<vector<int>>> &dp){
-    if(ri<0 || ci<0 || ri>=row || ci>=col || health<0) return false;
-    if(grid[ri][ci]==1) health--;
-    if(health<=0) return false;
-    if(ri==row-1 && ci==col-1) return true;
-    if(dp[ri][ci][health]!=-1) return dp[ri][ci][health];
-    int delr[]={-1,0,1,0};
-    int delc[]={0,1,0,-1};dp[ri][ci][health] = 0;
-    return dp[ri][ci][health]=(f(ri+delr[0],ci+delc[0],health,grid,row,col,dp) || f(ri+delr[1],ci+delc[1],health,grid,row,col,dp) || f(ri+delr[2],ci+delc[2],health,grid,row,col,dp) || f(ri+delr[3],ci+delc[3],health,grid,row,col,dp));
-}
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        int row=grid.size(), col=grid[0].size();
-        vector<vector<vector<int>>> dp(row, vector<vector<int>>(col, vector<int>(health + 1, -1)));
-        return f(0,0,health,grid,row,col,dp);
+        int row = grid.size(), col = grid[0].size();
+        queue<pair<int, pair<int, int>>> q;
+        if (grid[0][0] == 1)
+            health--;
+        if (health <= 0)
+            return false; // 1. MANDATORY: Track max health per cell to stop the
+                          // infinite loops causing your MLE
+        vector<vector<int>> maxh(row, vector<int>(col, -1));
+        q.push({health, {0, 0}});
+        maxh[0][0] = health;
+        while (!q.empty()) {
+            int h = q.front().first;
+            int ri = q.front().second.first;
+            int ci = q.front().second.second;
+            int delr[] = {-1, 0, 1, 0};
+            int delc[] = {0, 1, 0, -1};
+            q.pop();
+            if (ri == row - 1 && ci == col - 1 && h >= 1)
+                return true;
+            for (int i = 0; i < 4; i++) {
+                int newr = delr[i] + ri;
+                int newc = delc[i] + ci;
+                if (newr < row && newr >= 0 && newc < col && newc >= 0) {
+                    int next_h = h - grid[newr][newc];
+
+                    if (next_h > maxh[newr][newc] && next_h > 0) {
+                        maxh[newr][newc] = next_h;
+                        q.push({next_h, {newr, newc}});
+                    }
+                }
+            }
+        }
+        return false;
     }
 };
