0ed75c896f751be19312d16968bc00f2640f6a3d
Krish Prasad <prasadkrish9306@gmail.com>
2026-07-31 23:29:18 +0530

    Time: 22 ms (28.67%), Space: 25.4 MB (84.45%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -7,10 +7,20 @@ class Solution {
         auto cmp = [](const int& a,const int& b){
             return a < b;
         };
-        sort(freq.begin(),freq.end(),cmp);
-        for(int f : freq){
-            cout << f << "\t";
+        int n = 10;
+        vector<int> q(n,0);
+        for(int i =0;i<n;i++){
+            q[i] = i;
         }
+        sort(q.begin(),q.end(),cmp);
+        for(int k : q){
+            cout << k << "\t";
+        }
+        cout << endl;
+        sort(freq.begin(),freq.end(),cmp);
+        // for(int f : freq){
+        //     cout << f << "\t";
+        // }
         cout << endl;
         int ans = 0;
         for (int i = 25; i >= 0 && freq[i] > 0; i--) 
@@ -19,3 +29,10 @@ class Solution {
         return ans;
     }
 };
+
+
+// auto cmp = [](){
+//     return a>b ; a has lesser pq
+// }
+// a<b b has lesser
+// pb<int,vector<int>,decltype(cmp)> pq(cmp);
