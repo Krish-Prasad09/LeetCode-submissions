3f2eab3a196d09f91488fc5fe68e997c081f3401
Krish Prasad <prasadkrish9306@gmail.com>
2026-08-27 17:37:18 +0530

    Time: 8 ms (71.11%), Space: 38 MB (5.53%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n==1){
+            if(nums[0]==k) return 1;
+            else return 0;
+        }
+        int cnt=0;
+        vector<int> pref;
+        pref.push_back(0);
+        int prefS=0;
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        for(int i=0; i<n; i++){
+            prefS+=nums[i];
+            pref.push_back(prefS);
+        }
+        for(int i=1; i<pref.size(); i++){
+            if(mp.find(pref[i]-k) !=mp.end()){
+                cnt += mp[pref[i]-k];
+            }
+            mp[pref[i]]++;
+        }
+
+        return cnt;
+    }
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n==1){
+            if(nums[0]%k==0) return 1;
+            else return 0;
+        }
+        int cnt=0;
+        vector<int> pref;
+        pref.push_back(0);
+        int prefS=0;
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        for(int i=0; i<n; i++){
+            prefS+=nums[i];
+            pref.push_back(prefS);
+        }
+        for(int i=1; i<pref.size(); i++){
+            int rem = pref[i] % k;
+
+            if(rem < 0)
+                rem += k;
+
+            if(mp.find(rem) != mp.end())
+                cnt += mp[rem];
+
+            mp[rem]++;
+        }
+
+        return cnt;
+    }
+};
