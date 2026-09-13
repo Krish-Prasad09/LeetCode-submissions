4e9cf1cbdd9072dc4212204217da98704523821d
Krish Prasad <prasadkrish9306@gmail.com>
2026-09-13 12:25:03 +0530

    Time: 989 ms (5.38%), Space: 421.7 MB (5.61%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -2,25 +2,75 @@ class Solution {
 public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         int n = img1.size();
-        vector<vector<int>> shifts(2 * n, vector<int>(2 * n, 0));
-        int maxOverlap = 0;
-        for (int r1 = 0; r1 < n; r1++) {
-            for (int c1 = 0; c1 < n; c1++) {
-                if (img1[r1][c1] != 1) continue;
-                for (int r2 = 0; r2 < n; r2++) {
-                    for (int c2 = 0; c2 < n; c2++) {
-                        if (img2[r2][c2] != 1) continue;
-                        int rowShift = n + r1 - r2;
-                        int colShift = n + c1 - c2;
-                        shifts[rowShift][colShift]++;
-                        maxOverlap = max(
-                            maxOverlap,
-                            shifts[rowShift][colShift]
-                        );
-                    }
+        int size = 3 * n;
+        
+        // CHANGED: vector<bitset<90>> shrinks the memory footprint by almost 98% 
+        // compared to vector<vector<int>> and removes heap overhead.
+        vector<bitset<90>> grid(size);
+        
+        int a1 = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(img1[i][j] == 1) {
+                    grid[i + n][j + n] = 1;
+                    a1++;
                 }
             }
         }
-        return maxOverlap;
+        
+        queue<tuple<vector<bitset<90>>, int, int, int>> q;
+        q.push({grid, a1, n, n});
+        
+        vector<vector<bool>> visited(size, vector<bool>(size, false));
+        visited[n][n] = true;
+        
+        int max_overlap = 0;
+        
+        int delr[] = {-1, 0, 1, 0};
+        int delc[] = {0, 1, 0, -1};
+        
+        while(!q.empty()){
+            auto [vec, ones, curr_x, curr_y] = q.front();
+            q.pop();
+            
+            int current_overlap = 0;
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    // Works identically to your original code
+                    if(vec[i + n][j + n] == 1 && img2[i][j] == 1){
+                        current_overlap++;
+                    }
+                }
+            }
+            max_overlap = max(max_overlap, current_overlap);
+            
+            for(int k = 0; k < 4; k++){
+                int nx = curr_x + delr[k];
+                int ny = curr_y + delc[k];
+                
+                if(nx >= 0 && nx < size && ny >= 0 && ny < size && !visited[nx][ny]){
+                    
+                    vector<bitset<90>> next_vec(size);
+                    
+                    for(int i = 0; i < size; i++){
+                        for(int j = 0; j < size; j++){
+                            if(vec[i][j] == 1){
+                                int ni = i + delr[k];
+                                int nj = j + delc[k];
+                                
+                                if(ni >= 0 && ni < size && nj >= 0 && nj < size){
+                                    next_vec[ni][nj] = 1;
+                                }
+                            }
+                        }
+                    }
+                    
+                    visited[nx][ny] = true;
+                    q.push({next_vec, ones, nx, ny});
+                }
+            }
+        }
+        
+        return max_overlap;
     }
 };
